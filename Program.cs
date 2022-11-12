c5f8760cc8f305d21ab09c0428a94c5661c8b9ae
shock-64-programmer <118068929+shock-64-programmer@users.noreply.github.com>
2022-11-12 20:07:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace цикл_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i<9; i++)
+            {
+                if (i == 5)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
